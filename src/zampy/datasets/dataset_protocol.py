4af8789a74d18971008732a5141bdc7362b25cfe
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2023-05-26 15:03:47 +0200

    Implement ingest & load for the ETH canopy height dataset (#7)
    * Add pint, dask to dependencies
    
    * add validation module
    
    * add variable reference
    
    * rework protocol
    
    * Add ingest method for ETH canopy height data
    
    * Make protocol methods abstract methods
    
    * Implement ETH load, add time dim+coords
    
    * Move docstrings to Dataset class definition.
    
    * Add notebook to demo dataset functionality.
    
    * Remove test file
    
    * Add validators to the Time- and SpatialBounds
    
    * Correct validators

--- src/zampy/datasets/dataset_protocol.py
+++ src/zampy/datasets/dataset_protocol.py
@@ -1,6 +1,10 @@
 """Outline of the dataset protocol."""
+import json
+import shutil
+from abc import abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Any
 from typing import List
 from typing import Optional
 from typing import Protocol
@@ -9,12 +13,15 @@ import numpy as np
 import xarray as xr
 
 
+FNAME_PROPERTIES = "properties.json"
+
+
 @dataclass
 class Variable:
     """zampy variable."""
 
     name: str
-    unit: str
+    unit: Any  # pint unit. typing has issues with pint 0.21
     desc: Optional[str] = ""
 
 
@@ -27,6 +34,19 @@ class SpatialBounds:
     south: float
     west: float
 
+    def __post_init__(self) -> None:
+        """Validate the initialized SpatialBounds class."""
+        if self.south > self.north:
+            raise ValueError(
+                "Value of southern bound is greater than norther bound."
+                "\nPlease check the spatial bounds input."
+            )
+        if self.west > self.east:
+            raise ValueError(
+                "Value of western bound is greater than east bound."
+                "\nPlease check the spatial bounds input."
+            )
+
 
 @dataclass
 class TimeBounds:
@@ -38,9 +58,20 @@ class TimeBounds:
     start: np.datetime64
     end: np.datetime64
 
+    def __post_init__(self) -> None:
+        """Validate the initialized TimeBounds class."""
+        if self.end < self.start:
+            raise ValueError("Start time should be smaller than end time.")
+
 
 class Dataset(Protocol):
-    """Dataset."""
+    """Zampy Dataset.
+
+    Methods:
+        download: Download data from this dataset.
+        ingest: Convert the raw data to a CF-compliant format.
+        load: Load the data into an xarray Dataset.
+    """
 
     name: str
     time_bounds: TimeBounds
@@ -56,6 +87,7 @@ class Dataset(Protocol):
         """Init."""
         ...
 
+    @abstractmethod
     def download(  # noqa: PLR0913
         self,
         download_dir: Path,
@@ -66,21 +98,128 @@ class Dataset(Protocol):
     ) -> bool:
         """Download the data.
 
+        Args:
+            download_dir: Path to the Zampy download directory.
+            time_bounds: The start and end time of the data that should be loaded.
+            spatial_bounds: The lat/lon bounding box for which the data should be
+                loaded.
+            variable_names: Which variables should be loaded.
+            overwrite: Overwrite existing files instead of skipping them.
+
         Returns:
             Download success
         """
         ...
 
-    def preprocess(
+    @abstractmethod
+    def ingest(
         self,
         download_dir: Path,
-        spatial_bounds: SpatialBounds,
-        time_bounds: TimeBounds,
-        variable_names: List[str],
+        ingest_dir: Path,
+        overwrite: bool = False,
     ) -> bool:
-        """Preprocess the downloaded data to the CF-like Zampy convention."""
+        """Convert the downloaded data to the CF-like Zampy convention.
+
+        Args:
+            download_dir: Path to the Zampy download directory.
+            ingest_dir: Path to the Zampy ingest directory.
+            overwrite: Overwrite existing files instead of skipping them.
+
+        Returns:
+            Ingest succes.
+        """
         ...
 
-    def load(self) -> xr.Dataset:
-        """Get the dataset as an xarray Dataset."""
+    @abstractmethod
+    def load(
+        self,
+        ingest_dir: Path,
+        time_bounds: TimeBounds,
+        spatial_bounds: SpatialBounds,
+        variable_names: List[str],
+    ) -> xr.Dataset:
+        """Get the dataset as an xarray Dataset.
+
+        Args:
+            ingest_dir: Path to the Zampy ingest directory.
+            time_bounds: The start and end time of the data that should be loaded.
+            spatial_bounds: The lat/lon bounding box for which the data should be
+                loaded.
+            variable_names: Which variables should be loaded.
+
+        Returns:
+            The desired data loaded as an xarray Dataset.
+        """
         ...
+
+
+def write_properties_file(
+    dataset_folder: Path,
+    spatial_bounds: SpatialBounds,
+    time_bounds: TimeBounds,
+    variable_names: List[str],
+) -> None:
+    """Write the (serialized) spatial and time bounds to a json file.
+
+    Args:
+        dataset_folder: Path to the dataset folder (download/preprocessing).
+        spatial_bounds: Spatial bounds of the data.
+        time_bounds: Time bounds of the data.
+        variable_names: The (standard) variable names of the data.
+    """
+    # Data to be written
+    json_dict = {
+        "start_time": str(time_bounds.start),
+        "end_time": str(time_bounds.end),
+        "north": spatial_bounds.north,
+        "east": spatial_bounds.east,
+        "south": spatial_bounds.south,
+        "west": spatial_bounds.west,
+        "variable_names": variable_names,
+    }
+
+    json_object = json.dumps(json_dict, indent=4)
+
+    with (dataset_folder / FNAME_PROPERTIES).open(mode="w", encoding="utf-8") as file:
+        file.write(json_object)
+
+
+def read_properties_file(
+    dataset_folder: Path,
+) -> Tuple[SpatialBounds, TimeBounds, List[str]]:
+    """Load the serialized spatial and time bounds from the json file.
+
+    Args:
+        dataset_folder: Path to the dataset folder (download/preprocessing).
+
+    Returns:
+        Tuple[SpatialBounds, TimeBounds]: The spatial and time bounds of the data.
+    """
+    with (dataset_folder / FNAME_PROPERTIES).open(mode="r", encoding="utf-8") as file:
+        json_dict = json.load(file)
+
+    return (
+        SpatialBounds(
+            json_dict["north"],
+            json_dict["east"],
+            json_dict["south"],
+            json_dict["west"],
+        ),
+        TimeBounds(start=json_dict["start_time"], end=json_dict["end_time"]),
+        json_dict["variable_names"],
+    )
+
+
+def copy_properties_file(
+    source_folder: Path,
+    target_folder: Path,
+) -> None:
+    """Copy the properties file from one folder to another.
+
+    To be used when, for example, the downloaded data has been ingested.
+
+    Args:
+        source_folder: Source folder containing the properties file.
+        target_folder: Destination folder where the file should be copied to.
+    """
+    shutil.copy(source_folder / FNAME_PROPERTIES, target_folder / FNAME_PROPERTIES)
--- src/zampy/datasets/eth_canopy_height.py
+++ src/zampy/datasets/eth_canopy_height.py
@@ -3,12 +3,15 @@ import gzip
 from pathlib import Path
 from typing import List
 import numpy as np
+import xarray as xr
 from zampy.datasets import utils
 from zampy.datasets import validation
 from zampy.datasets.dataset_protocol import Dataset
 from zampy.datasets.dataset_protocol import SpatialBounds
 from zampy.datasets.dataset_protocol import TimeBounds
 from zampy.datasets.dataset_protocol import Variable
+from zampy.datasets.dataset_protocol import copy_properties_file
+from zampy.datasets.dataset_protocol import write_properties_file
 from zampy.reference.variables import VARIABLE_REFERENCE_LOOKUP
 from zampy.reference.variables import unit_registry
 
@@ -17,11 +20,12 @@ VALID_NAME_FILE = (
     Path(__file__).parent / "assets" / "h_canopy_filenames_compressed.txt.gz"
 )
 
+## Ignore missing class/method docstrings: they are implemented in the Dataset class.
+# ruff: noqa: D102
 
-class EthCanopyHeight(Dataset):
-    """The ETH canopy height dataset."""
 
-    name = "ETH-canopy-height"
+class EthCanopyHeight(Dataset):  # noqa: D101
+    name = "eth-canopy-height"
     time_bounds = TimeBounds(np.datetime64("2020-01-01"), np.datetime64("2020-12-31"))
     spatial_bounds = SpatialBounds(90, 180, -90, -180)
     crs = "EPSG:4326"
@@ -33,7 +37,7 @@ class EthCanopyHeight(Dataset):
     variable_names = ("canopy-height", "canopy-height-standard-deviation")
     variables = (
         VARIABLE_REFERENCE_LOOKUP[var] for var in variable_names
-    )  #  type: ignore
+    )  # type: ignore
 
     license = "cc-by-4.0"
     bib = """
@@ -56,7 +60,6 @@ class EthCanopyHeight(Dataset):
         variable_names: List[str],
         overwrite: bool = False,
     ) -> bool:
-        """Download the ETH tiles to the download directory."""
         validation.validate_download_request(
             self,
             download_dir,
@@ -79,17 +82,62 @@ class EthCanopyHeight(Dataset):
                 fpath=download_folder / fname,
                 overwrite=overwrite,
             )
+
+        write_properties_file(
+            download_folder, spatial_bounds, time_bounds, variable_names
+        )
+
         return True
 
-    def preprocess(
+    def ingest(
         self,
         download_dir: Path,
-        spatial_bounds: SpatialBounds,
-        time_bounds: TimeBounds,
-        variable_names: List[str],
+        ingest_dir: Path,
+        overwrite: bool = False,
     ) -> bool:
-        """Preprocess the downloaded data to the CF-like Zampy convention."""
-        return False
+        download_folder = download_dir / self.name
+        ingest_folder = ingest_dir / self.name
+        ingest_folder.mkdir(parents=True, exist_ok=True)
+
+        data_file_pattern = "ETH_GlobalCanopyHeight_10m_2020_*_Map.tif"
+        sd_file_pattern = "ETH_GlobalCanopyHeight_10m_2020_*_Map_SD.tif"
+
+        data_files = list(download_folder.glob(data_file_pattern))
+        sd_files = list(download_folder.glob(sd_file_pattern))
+        is_sd_file = len(data_files) * [False] + len(sd_files) * [True]
+
+        for file, sd_file in zip(data_files + sd_files, is_sd_file):
+            convert_tiff_to_netcdf(
+                ingest_folder,
+                file=file,
+                sd_file=sd_file,
+                overwrite=overwrite,
+            )
+
+        copy_properties_file(download_folder, ingest_folder)
+
+        return True
+
+    def load(
+        self,
+        ingest_dir: Path,
+        time_bounds: TimeBounds,
+        spatial_bounds: SpatialBounds,
+        variable_names: List[str],
+    ) -> xr.Dataset:
+        files: List[Path] = []
+        if self.variable_names[0] in variable_names:
+            files += (ingest_dir / self.name).glob("*Map.nc")
+        if self.variable_names[1] in variable_names:
+            files += (ingest_dir / self.name).glob("*Map_SD.nc")
+
+        ds = xr.open_mfdataset(files, chunks={"latitude": 6000, "longitude": 6000})
+        ds = ds.sel(
+            latitude=slice(spatial_bounds.south, spatial_bounds.north),
+            longitude=slice(spatial_bounds.west, spatial_bounds.east),
+            time=slice(time_bounds.start, time_bounds.end),
+        )
+        return ds
 
 
 def get_filenames(bounds: SpatialBounds, sd_file: bool = False) -> List[str]:
@@ -107,8 +155,8 @@ def get_filenames(bounds: SpatialBounds, sd_file: bool = False) -> List[str]:
     step = 3
 
     locs = np.meshgrid(
-        np.arange(start=bounds.south, stop=bounds.north + step, step=step),
-        np.arange(start=bounds.west, stop=bounds.east + step, step=step),
+        np.arange(start=bounds.south, stop=bounds.north, step=step),
+        np.arange(start=bounds.west, stop=bounds.east, step=step),
     )
     lats = locs[0].flatten()
     lons = locs[1].flatten()
@@ -143,3 +191,80 @@ def get_valid_filenames(filenames: List[str]) -> List[str]:
         if fname.replace("_SD", "") in valid_filenames:
             valid_names.append(fname)
     return valid_names
+
+
+def convert_tiff_to_netcdf(
+    ingest_folder: Path,
+    file: Path,
+    sd_file: bool = False,
+    overwrite: bool = False,
+) -> None:
+    """Convert the downloaded tiff files to standard CF/Zampy netCDF files.
+
+    Args:
+        ingest_folder: Folder where the files have to be written to.
+        file: Path to the ETH canopy height tiff file.
+        sd_file: If the file contains the standard deviation values.
+        overwrite: Overwrite all existing files. If False, file that already exist will
+            be skipped.
+    """
+    ncfile = ingest_folder / file.with_suffix(".nc").name
+    if ncfile.exists() and not overwrite:
+        print(f"File '{ncfile.name}' already exists, skipping...")
+    else:
+        ds = parse_tiff_file(file, sd_file)
+        ds.to_netcdf(
+            path=ncfile,
+            encoding=ds.encoding,
+        )
+
+
+def parse_tiff_file(file: Path, sd_file: bool = False) -> xr.Dataset:
+    """Parse the downloaded canopy height tiff files, to CF/Zampy standard dataset.
+
+    Args:
+        file: Path to the ETH canopy height tiff file.
+        sd_file: If the file contains the standard deviation values.
+
+    Returns:
+        CF/Zampy formatted xarray Dataset
+    """
+    # Open chunked: will be dask array -> file writing can be parallelized.
+    da = xr.open_dataarray(file, engine="rasterio", chunks={"x": 6000, "y": 6000})
+    da = da.sortby(["x", "y"])  # sort the dims ascending
+    da = da.isel(band=0)  # get rid of band dim
+    da = da.drop_vars(["band", "spatial_ref"])  # drop unnecessary coords
+    ds = da.to_dataset()
+    ds = ds.assign_coords({"time": np.datetime64("2020-07-01")})  # halfway in the year
+    ds = ds.expand_dims("time")
+    ds = ds.rename(
+        {
+            "band_data": "canopy-height-standard-deviation"
+            if sd_file
+            else "canopy-height",
+            "x": "longitude",
+            "y": "latitude",
+        }
+    )
+
+    # All eth variables & coords already follow the CF/Zampy convention
+    for variable in ds.variables:
+        variable_name = str(variable)  # Cast to string to please mypy.
+        if variable_name != "time":  # TODO: Check how to write time attrs...
+            ds[variable_name].attrs["units"] = str(
+                VARIABLE_REFERENCE_LOOKUP[variable_name].unit
+            )
+            ds[variable_name].attrs["description"] = VARIABLE_REFERENCE_LOOKUP[
+                variable_name
+            ].desc
+
+    # TODO: add dataset attributes.
+    ds.encoding = {
+        "canopy-height-standard-deviation"
+        if sd_file
+        else "canopy-height": {
+            "zlib": True,
+            "complevel": 5,
+        }
+    }
+    return ds
