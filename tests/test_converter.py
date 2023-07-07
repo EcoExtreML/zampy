b3c9a15ec18e4c4e74e10f8d4b0259a6da81f6ec
Yang <y.liu@esciencecenter.nl>
2023-07-07 11:16:41 +0200

    further improve coverage for converter

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 import numpy as np
 import pytest
+import xarray as xr
 from zampy.datasets import EthCanopyHeight
 from zampy.datasets import converter
 from zampy.datasets.eth_canopy_height import parse_tiff_file
@@ -25,18 +26,8 @@ def test_check_convention_not_exist():
         converter.check_convention(convention)
 
 
-def test_convert():
-    ds = parse_tiff_file(
-        path_dummy_data / "ETH_GlobalCanopyHeight_10m_2020_N51E003_Map.tif",
-    )
-    ds_convert = converter.convert(
-        data=ds, dataset=EthCanopyHeight(), convention="ALMA"
-    )
-
-    assert list(ds_convert.data_vars)[0] == "Hveg"
-
-
 def test_convert_var():
+    """Test _convert_var function."""
     ds = parse_tiff_file(
         path_dummy_data / "ETH_GlobalCanopyHeight_10m_2020_N51E003_Map.tif",
     )
@@ -47,3 +38,65 @@ def test_convert_var():
         ds["height_of_vegetation"].values * 10.0,
         equal_nan=True,
     )
+
+
+def test_convert_var_name():
+    """Test convert function.
+
+    In this test, no conversion is performed. Only the variable name is updated.
+    """
+    ds = parse_tiff_file(
+        path_dummy_data / "ETH_GlobalCanopyHeight_10m_2020_N51E003_Map.tif",
+    )
+    ds_convert = converter.convert(
+        data=ds, dataset=EthCanopyHeight(), convention="ALMA"
+    )
+
+    assert list(ds_convert.data_vars)[0] == "Hveg"
+
+
+def test_convert_unit():
+    """Test convert function.
+
+    In this test, unit conversion is performed.
+    """
+    ds = parse_tiff_file(
+        path_dummy_data / "ETH_GlobalCanopyHeight_10m_2020_N51E003_Map.tif",
+    )
+    ds["height_of_vegetation"].attrs["units"] = "decimeter"
+    ds_convert = converter.convert(
+        data=ds, dataset=EthCanopyHeight(), convention="ALMA"
+    )
+
+    assert np.allclose(
+        ds_convert["Hveg"].values,
+        ds["height_of_vegetation"].values / 10.0,
+        equal_nan=True,
+    )
+
+
+def test_convert_no_conversion():
+    """Test convert function.
+
+    In this test, no conversion is performed. The input will be returned without change.
+    """
+    dummy_ds = xr.Dataset(
+        data_vars=dict(
+            temperature=(
+                ["latitude", "longitude"],
+                np.random.randn(1, 2),
+                {"units": "Celsius"},
+            ),
+        ),
+        coords=dict(
+            lon=(["longitude"], [110, 111]),
+            lat=(["latitude"], [20]),
+        ),
+        attrs=dict(units="Weather dataset."),
+    )
+
+    ds_convert = converter.convert(
+        data=dummy_ds, dataset=EthCanopyHeight(), convention="ALMA"
+    )
+
+    assert list(ds_convert.data_vars)[0] == "temperature"
