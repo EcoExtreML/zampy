313a14be5a776f195c1d8329d8358d6deec8d551
Yang <y.liu@esciencecenter.nl>
2023-09-13 10:59:14 +0200

    add downloading function and demo notebook

--- src/zampy/datasets/land_cover.py
+++ src/zampy/datasets/land_cover.py
@@ -1,9 +1,14 @@
 """Land cover classification dataset."""
 
+from pathlib import Path
 import numpy as np
+from zampy.datasets import cds_utils
+from zampy.datasets import validation
 from zampy.datasets.dataset_protocol import SpatialBounds
 from zampy.datasets.dataset_protocol import TimeBounds
 from zampy.datasets.dataset_protocol import Variable
+from zampy.datasets.dataset_protocol import copy_properties_file
+from zampy.datasets.dataset_protocol import write_properties_file
 from zampy.reference.variables import VARIABLE_REFERENCE_LOOKUP
 from zampy.reference.variables import unit_registry
 
@@ -14,6 +19,7 @@ from zampy.reference.variables import unit_registry
 
 class LandCover:  # noqa: D101
     """Land cover classification gridded maps."""
+
     name = "land-cover"
     time_bounds = TimeBounds(np.datetime64("1992-01-01"), np.datetime64("2020-12-31"))
     spatial_bounds = SpatialBounds(90, 180, -90, -180)
@@ -30,7 +36,7 @@ class LandCover:  # noqa: D101
     bib = """
     @article{buchhorn2020copernicus,
     title={Copernicus global land cover layers—collection 2},
-    author={Buchhorn, Marcel and Lesiv, Myroslava and Tsendbazar, Nandin-Erdene and Herold, Martin and Bertels, Luc and Smets, Bruno},
+    author={Buchhorn, Marcel et al.},
     journal={Remote Sensing},
     volume={12},
     number={6},
@@ -43,3 +49,39 @@ class LandCover:  # noqa: D101
     data_url = "https://cds.climate.copernicus.eu/cdsapp#!/dataset/satellite-land-cover?tab=overview"
 
     cds_dataset = "satellite-land-cover"
+
+    def __init__(self) -> None:
+        """Init."""
+        pass
+
+    def download(
+        self,
+        download_dir: Path,
+        time_bounds: TimeBounds,
+        spatial_bounds: SpatialBounds,
+        variable_names: list[str],
+        overwrite: bool = False,
+    ) -> bool:
+        validation.validate_download_request(
+            self,
+            download_dir,
+            time_bounds,
+            spatial_bounds,
+            variable_names,
+        )
+
+        download_folder = download_dir / self.name
+        download_folder.mkdir(parents=True, exist_ok=True)
+
+        cds_utils.cds_request_land_cover(
+            dataset=self.cds_dataset,
+            time_bounds=time_bounds,
+            path=download_folder,
+            overwrite=overwrite,
+        )
+
+        write_properties_file(
+            download_folder, spatial_bounds, time_bounds, variable_names
+        )
+
+        return True
