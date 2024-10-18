47b57ce4d1adec4a89f5cc8f3c30e92cb5fca227
SarahAlidoost <f.alidoost@esciencecenter.nl>
2024-10-18 16:32:51 +0200

    fix version in land_cover cds request

--- src/zampy/datasets/cds_utils.py
+++ src/zampy/datasets/cds_utils.py
@@ -2,6 +2,8 @@
 
 from copy import copy
 from pathlib import Path
+
+import requests
 import cdsapi
 import numpy as np
 import pandas as pd
@@ -35,10 +37,10 @@ ALL_DAYS = [
     "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
     "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
     "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
-    "31", 
+    "31",
 ]  # fmt: skip
 
-ALL_HOURS = [  
+ALL_HOURS = [
     "00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00",
     "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00",
     "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00",
@@ -154,9 +156,9 @@ def cds_request_land_cover(
 
     for year in tqdm(years):
         if int(year) < 2016:
-            version = "v2.0.7cds"
+            version = "v2_0_7cds"
         else:
-            version = "v2.1.1"
+            version = "v2_1_1"
         r = c.retrieve(
             dataset,
             {
