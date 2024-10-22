1eab581294bf96c79c512b83274420471182f83e
SarahAlidoost <f.alidoost@esciencecenter.nl>
2024-10-22 13:59:58 +0200

    remove unused import from test_recipe

--- tests/test_recipes/test_simple_recipe.py
+++ tests/test_recipes/test_simple_recipe.py
@@ -13,7 +13,6 @@ from zampy.datasets.dataset_protocol import TimeBounds
 from zampy.datasets.dataset_protocol import write_properties_file
 from zampy.recipe import RecipeManager
 from zampy.recipe import convert_time
-import pandas as pd
 
 
 RECIPE_FILE = Path(__file__).parent / "recipes" / "era5_recipe.yml"
